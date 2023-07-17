6a6a8451bb49eb2bfa2408acdfe4e49c59c09ffb
OTumanov <otumanov@gmail.com>
2023-07-17 14:58:29 +0300

    - починил репозиторий ))

--- stats-server/src/main/java/ru/practicum/stsvc/dto/UtilDto.java
+++ stats-server/src/main/java/ru/practicum/stsvc/dto/UtilDto.java
@@ -1,13 +1,30 @@
 package ru.practicum.stsvc.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class UtilDto {
     private Long entityId;
     private Long count;
+
+    public UtilDto(Long entityId, Long count) {
+        this.entityId = entityId;
+        this.count = count;
+    }
+
+    public UtilDto() {
+    }
+
+    public Long getEntityId() {
+        return entityId;
+    }
+
+    public void setEntityId(Long entityId) {
+        this.entityId = entityId;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
 }
